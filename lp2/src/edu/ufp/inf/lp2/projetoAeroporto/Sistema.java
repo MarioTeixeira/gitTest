cbebc400d5f21eb580dd8640d797b0f5f09721d4
Mário Teixeira <mario.teixeira@oracle.com>
2019-10-12 19:59:25 +0100

    Revert "comment some code"
    This reverts commit d395f321c99ea416b664a9b002771dd0b35e6d14.

--- lp2/src/edu/ufp/inf/lp2/projetoAeroporto/Sistema.java
+++ lp2/src/edu/ufp/inf/lp2/projetoAeroporto/Sistema.java
@@ -89,6 +89,11 @@ public class Sistema{
 
      public Vector <Aviao> aviaoNoAeroporto(String sigla){
            Vector <Aviao> vav=new Vector();
+            
+         
+
+
+
 
            Iterator<String> itr = this.avioes.keys().iterator();
             while (itr.hasNext()) {
@@ -103,7 +108,9 @@ public class Sistema{
 
 
                 
-
+       
+            
+     
     public void remAeroporto(String sigla) {
         this.aeroportos.delete(sigla);
     }
@@ -194,11 +201,11 @@ public class Sistema{
 
      
          
-//
-//         System.out.println(gr.searchAviao("Eusebio"));
-//          System.out.println(gr.searchAeroporto("OPO"));
-//           System.out.println(gr.aviaoNoAeroporto("OPO"));
-//      
+         
+         System.out.println(gr.searchAviao("Eusebio"));
+          System.out.println(gr.searchAeroporto("OPO"));
+           System.out.println(gr.aviaoNoAeroporto("OPO"));
+      
     }
 
     /**
